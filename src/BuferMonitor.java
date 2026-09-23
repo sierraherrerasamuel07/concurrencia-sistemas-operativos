9216e65f58365bbd6fd71f476331f29814c0a2d4
sierraherrerasamuel07 <sierraherrerasamuel07@gmail.com>
2026-09-23 00:20:15 -0500

    Update BuferMonitor.java

--- src/BuferMonitor.java
+++ src/BuferMonitor.java
@@ -1,14 +1,6 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-/**
- * Implementacion del bufer compartido usando un MONITOR: la palabra clave
- * "synchronized" de Java convierte cada metodo en una region critica donde
- * solo un hilo puede estar a la vez. wait() y notifyAll() son las VARIABLES
- * DE CONDICION que permiten a un hilo "dormirse" cuando la condicion que
- * necesita (bufer no lleno / no vacio) todavia no se cumple, y despertar
- * a los demas cuando la condicion cambia.
- */
 public class BuferMonitor implements BuferCompartido {
 
     private final Queue<Integer> cola = new LinkedList<>();
@@ -20,25 +12,21 @@ public class BuferMonitor implements BuferCompartido {
 
     @Override
     public synchronized void producir(int dato) throws InterruptedException {
-        // Mientras el bufer este lleno, el productor se duerme aqui.
         while (cola.size() == capacidad) {
             wait();
         }
         cola.add(dato);
         System.out.println("[Monitor] Productor inserto " + dato + " (tamaño bufer=" + cola.size() + ")");
-        // Avisa a los consumidores dormidos que ya hay algo nuevo que consumir.
         notifyAll();
     }
 
     @Override
     public synchronized int consumir() throws InterruptedException {
-        // Mientras el bufer este vacio, el consumidor se duerme aqui.
         while (cola.isEmpty()) {
             wait();
         }
         int dato = cola.poll();
         System.out.println("[Monitor] Consumidor retiro " + dato + " (tamaño bufer=" + cola.size() + ")");
-        // Avisa a los productores dormidos que ya hay espacio libre.
         notifyAll();
         return dato;
     }
