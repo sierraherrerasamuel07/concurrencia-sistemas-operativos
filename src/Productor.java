08cae443c7f9489a1af9d1e42cdd54c1d3656f07
sierraherrerasamuel07 <sierraherrerasamuel07@gmail.com>
2026-09-23 00:21:42 -0500

    Update Productor.java

--- src/Productor.java
+++ src/Productor.java
@@ -1,8 +1,3 @@
-/**
- * Hilo que genera datos y los inserta en el bufer compartido.
- * No sabe (ni le importa) que mecanismo de sincronizacion usa el bufer
- * por dentro, porque solo conoce la interfaz BuferCompartido.
- */
 public class Productor extends Thread {
 
     private final BuferCompartido bufer;
@@ -21,7 +16,7 @@ public class Productor extends Thread {
             try {
                 int dato = Integer.parseInt(nombre.replaceAll("[^0-9]", "")) * 100 + i;
                 bufer.producir(dato);
-                Thread.sleep(50); // simula que producir algo toma un poco de tiempo
+                Thread.sleep(50);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 System.out.println(nombre + " fue interrumpido.");
